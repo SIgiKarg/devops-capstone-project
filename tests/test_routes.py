47de861c3668373201843274b2232aa2c7375041
SIgiKarg <sfk@zurich.ibm.com>
2024-06-18 10:08:44 -0400

    delete accounts implemented

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -171,4 +171,26 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)        
 
+    def test_delete_account(self):
+        """It should Delete an Account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.delete(f"{BASE_URL}/{account.id}")
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_delete_account_not_found(self):
+        """It should Do Nothing when Delete not found Account"""
+        self._create_accounts(1)[0]
+        response = self.client.delete(f"{BASE_URL}/0")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        list_response = self.client.get(
+            BASE_URL, content_type="application/json"
+        )
+        accounts = list_response.get_json()
+        self.assertEqual(len(accounts), 1)
+
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        response = self.client.delete(BASE_URL)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)        
     # ADD YOUR TEST CASES HERE ...
